81c7e44147e3aafc50aa6239c56445c5e50ce9be
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-02-25 23:32:38 +0700

    test

--- actions/with-actions-toolkit-action/src/index.ts
+++ actions/with-actions-toolkit-action/src/index.ts
@@ -1,9 +1,19 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-core.info('start')
+core.info("start");
 core.info(JSON.stringify(github.context, null, 2));
-core.info('end')
-core.exportVariable('MY_ENV_1', '123');
-core.setSecret('$MY_ENV_1');
+core.info("end");
+core.warning("core warning");
+core.error("core error");
+core.exportVariable("MY_ENV_1", "123");
+core.setSecret("$MY_ENV_1");
 // core.setSecret('MY_ENV_1');
+
+try {
+  // Do stuff
+  throw new Error("something.");
+} catch (err) {
+  // setFailed logs the message and sets a failing exit code
+  core.setFailed(`Action failed with error ${err}`);
+}
