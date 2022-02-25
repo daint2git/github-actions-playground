7afba38639ab415f528cbc0850766d501ad5a959
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-02-25 22:56:28 +0700

    test

--- actions/with-actions-toolkit-action/src/index.ts
+++ actions/with-actions-toolkit-action/src/index.ts
@@ -1,4 +1,5 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-core.info(JSON.stringify(github.context, null, 2))
+core.info(JSON.stringify(github.context, null, 2));
+console.info("console", JSON.stringify(github.context, null, 2));
