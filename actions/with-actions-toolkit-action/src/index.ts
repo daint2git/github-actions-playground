045d30329841b470d16df4e4f11e2f761d8b8377
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-02-25 23:08:09 +0700

    test

--- actions/with-actions-toolkit-action/src/index.ts
+++ actions/with-actions-toolkit-action/src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 core.info('start')
-core.info(JSON.stringify(github.context, null, 2));
+// core.info(JSON.stringify(github.context, null, 2));
 // console.info("khong co", JSON.stringify(github.context, null, 2));
 core.info('end')
 core.exportVariable('MY_ENV_1', '123');
