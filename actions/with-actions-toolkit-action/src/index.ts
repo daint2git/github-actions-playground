2f4b529461f7861894792d3b6e9c98f06e269922
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-02-25 23:20:00 +0700

    test

--- actions/with-actions-toolkit-action/src/index.ts
+++ actions/with-actions-toolkit-action/src/index.ts
@@ -3,7 +3,6 @@ import * as github from "@actions/github";
 
 core.info('start')
 core.info(JSON.stringify(github.context, null, 2));
-// console.info("khong co", JSON.stringify(github.context, null, 2));
 core.info('end')
 core.exportVariable('MY_ENV_1', '123');
-// core.setSecret('myPassword');
+core.setSecret('$MY_ENV_1');
